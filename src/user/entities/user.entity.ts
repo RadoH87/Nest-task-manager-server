778cf39e13c25f48a81416fc7a57cef79bd1514d
Radek Holubowski <radekh8787@gmail.com>
2022-07-07 03:09:49 +0100

    Defined user entity and structure.

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 255,
+  })
+  firstName: string;
+
+  @Column({
+    length: 255,
+  })
+  lastName: string;
+
+  @Column({
+    length: 255,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+}
