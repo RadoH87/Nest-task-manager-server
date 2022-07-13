29028c5e72b54e2e435b53aa8e8c943ebb62d709
Radek Holubowski <radekh8787@gmail.com>
2022-07-13 19:27:19 +0100

    Added Auth module with local strategy authentication.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { TaskModule } from './task/task.module';
     }),
     UserModule,
     TaskModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
